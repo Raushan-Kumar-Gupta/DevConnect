60df60e7b360d1b3c039d0720cdcbe809a78484e
Raushan-Kumar-Gupta <raushankalwar98242@gmail.com>
2025-07-14 14:22:59 +0530

    completed header, footer component

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,15 +1,20 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./NavBar"
+import NavBar from "./NavBar";
 import Footer from "./Footer";
 
-const Body = ()=>{
-    return (
-        <div>
-            <NavBar/>
-            <Outlet/>
-            <Footer/>
-        </div>
-    )
-}
+const Body = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+
+      {/* This Outlet will grow and push the footer down */}
+      <div className="flex-1">
+        <Outlet />
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Body;
